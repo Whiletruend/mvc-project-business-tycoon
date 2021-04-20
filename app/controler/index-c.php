77a62ed348b124b6c1f30dc38285a0429a7c82ec
Benjamin Bongage <benjamin@MacBook-Air-de-Benjamin.local>
2021-04-20 21:31:39 +0200

    Create the index page (index.php with the model, view and controler). --ONLY FOR A TEST

---
+++ app/controler/index-c.php
@@ -0,0 +1,24 @@
+<?php
+    #-> Class 'Index'
+    class IndexControler {
+        private static $_instance = null;
+        private $test;
+        private $activePage;
+
+        private function __construct() {
+            $this->test = 'just a simple try';
+        }
+
+        public static function getInstance() {
+            if(is_null(self::$_instance)) {
+                self::$_instance = new IndexControler();
+            }
+
+            return self::$_instance;
+        }
+
+        public function render() {
+            include_once 'app/view/index-v.php';
+        }
+    }
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	require 'app/controler/index-c.php';
+
+	if(isset($_GET['action'])) {
+		$action = $_GET['action'];
+	} else {
+		$action = 'default';
+	}
+
+	switch($action) {
+		case 'test':
+			echo 'nothing';
+			break;
+		default:
+			IndexControler::getInstance()->render();
+			break;
+
+	}
+?>
